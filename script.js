88cedcd0d8fac81dac42aa26c456cead72e8ca4d
Matt Layton <matthew.layton@cleverbridge.com>
2021-10-25 23:46:33 -0500

    Add error handling when fetching from API

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Page element selectors
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
@@ -5,19 +6,24 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
+// Quotes
 let apiQuotes = [];
 
-function showLoadingSpinner() {
-  loader.hidden = false;
-  quoteContainer.hidden = true;
+async function fetchQuotesFromAPI() {
+  showLoadingSpinner();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+    throw new Error("oops");
+  } catch(error) {
+    // Catch error
+    console.error(error);
+    displayErrorMessage();
+  }
 }
 
-function removeLoadingSpinner() {
-  loader.hidden = true;
-  quoteContainer.hidden = false;
-}
-
-// Show new quote
 function newQuote() {
   // Get random quote from apiQuotes array
   const random = Math.floor(Math.random() * apiQuotes.length);
@@ -35,23 +41,36 @@ function newQuote() {
   removeLoadingSpinner();
 }
 
-// Get quotes from API
-async function getQuotes() {
-  showLoadingSpinner();
-  const apiUrl = "https://type.fit/api/quotes";
-  try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
-  } catch(error) {
-    // Catch error
-  }
-}
-
-// Tweet quote
 function tweetQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
   window.open(twitterUrl, '_blank');
+}
+
+function displayErrorMessage() {
+  // Remove all child nodes from quote container
+  while (quoteContainer.firstChild) {
+    quoteContainer.removeChild(quoteContainer.firstChild);
+  }
+
+  // Create error content
+  let errorContainer = document.createElement('div');
+  errorContainer.appendChild( document.createTextNode(
+    `An error occurred while fetching data. Please refresh the page or try again later. Sorry for the inconvenience!`
+  ) );
+  errorContainer.setAttribute('id', 'error');
+
+  // Add error content to quote container
+  quoteContainer.appendChild(errorContainer);
+}
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
 }
 
 // Event listeners
@@ -59,4 +78,4 @@ newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
-getQuotes();
+fetchQuotesFromAPI();
